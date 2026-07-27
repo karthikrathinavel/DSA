0a1eb61b37e12d8cc18e6ffae0cc1450514f4173
Karthik Rathinavel <80736284+karthikrathinavel@users.noreply.github.com>
2026-07-27 13:12:34 +0530

    Update two_sum_1.py

--- leetcode/two_sum_1.py
+++ leetcode/two_sum_1.py
@@ -1,12 +1,10 @@
-def twoSum(nums, target):
-        n = len(nums)
-        if n <= 1:
-            return nums
-        for i in range(n):
-            for j in range(i, n-1):
-                if nums[j] + nums[j+1] == target:
-                    return [j, j+1]
-                
-arr = [2,7,11,15]
-target = 9
-print(twoSum(arr, target))
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        res = set()
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if nums[i] + nums[j] == target and i != j:
+                    res.add(i)
+                    res.add(j)
+        return list(res)
+        
